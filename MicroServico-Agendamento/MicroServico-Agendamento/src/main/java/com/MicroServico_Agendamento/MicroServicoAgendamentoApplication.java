f32cf20deffdcbf3ff3f68c9b0547dbb6f9f4881
vitorLevi17 <vitorlevimenezessantos@outlook.com>
2025-11-06 23:54:31 -0300

    Criação do projeto

---
+++ MicroServico-Agendamento/MicroServico-Agendamento/src/main/java/com/MicroServico_Agendamento/MicroServicoAgendamentoApplication.java
@@ -0,0 +1,13 @@
+package com.MicroServico_Agendamento;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroServicoAgendamentoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroServicoAgendamentoApplication.class, args);
+	}
+
+}
---
+++ MicroServico-Agendamento/MicroServico-Agendamento/src/test/java/com/MicroServico_Agendamento/MicroServicoAgendamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MicroServico_Agendamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServicoAgendamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
